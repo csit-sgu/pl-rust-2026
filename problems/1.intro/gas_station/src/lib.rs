cf7509971cfb4ee9e0eee9303333b9c5e2bdb1e2
Kudyakov Artem <t3m8ch@yandex.ru>
2026-02-19 12:56:44 +0400

    refactor(gas_station): Поменять выходной тип на Option<u32>

--- problems/1.intro/gas_station/src/lib.rs
+++ problems/1.intro/gas_station/src/lib.rs
@@ -1,5 +1,5 @@
 #![forbid(unsafe_code)]
 
-pub fn can_complete_circuit(gas: &[i32], cost: &[i32]) -> i32 {
+pub fn can_complete_circuit(gas: &[i32], cost: &[i32]) -> Option<u32> {
     unimplemented!()
 }
